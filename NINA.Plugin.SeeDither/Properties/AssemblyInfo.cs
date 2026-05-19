52b92a1f95ac5b78abd0fc87391f6dd69abb424b
cstovi <cstovi@users.noreply.github.com>
2026-05-19 18:40:18 +0100

    Add Ko-fi support link to plugin page description, bump version to 1.1.0

--- NINA.Plugin.SeeDither/Properties/AssemblyInfo.cs
+++ NINA.Plugin.SeeDither/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c4d2e5a1-7b3f-4e9d-9f2a-6f1c8b9a3d11")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
 
 [assembly: AssemblyMetadata("Id", "c4d2e5a1-7b3f-4e9d-9f2a-6f1c8b9a3d11")]
 [assembly: AssemblyMetadata("Name", "SeeDither")]
@@ -24,4 +24,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("FeaturedImageURL", "https://i.ibb.co/v6C6vXjp/seedither.jpg")]
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 [assembly: AssemblyMetadata("AltScreenshotURL", "")]
-[assembly: AssemblyMetadata("LongDescription", "Performs random absolute-coordinate dithering after exposures, designed for Seestar mounts whose guide pulses are unreliable.")]
+[assembly: AssemblyMetadata("LongDescription", "Performs random absolute-coordinate dithering after exposures, designed for Seestar mounts whose guide pulses are unreliable.\n\nIf you use and like anything I've done, support on Ko-fi (https://ko-fi.com/turnpike47298) is appreciated to encourage me to keep going!")]
